ee434063ccd8168fedb6389f49eb450c86d44b39
sightem <sightem@national.shitposting.agency>
2022-11-26 06:31:52 +0100

    progress

--- OMPTSyntaxHighlight/Source.cpp
+++ OMPTSyntaxHighlight/Source.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <exception>
 #include <sstream>
+#include <regex>
 #include <Windows.h>
 
 struct CLIOptions
@@ -34,8 +35,8 @@ const std::string HELP_MESSAGE =
 
 const int DEFAULT_COLORS[] = { 7, 5, 4, 2, 6, 3, 1, 7 };
 const std::string HEADER = "ModPlug Tracker ";
-const std::string FORMATS_M[] = { "MOD", " XM" };
-const std::string FORMATS_S[] = { "S3M", " IT", "MPT" };
+std::vector<std::string> FORMATS_M = { "MOD", " XM" };
+std::vector<std::string> FORMATS_S = { "S3M", " IT", "MPT" };
 
 
 bool StartsWith(const char* pre, const char* str);
@@ -82,6 +83,68 @@ int main(int argc, char* argv[])
 			Colors[i] = DEFAULT_COLORS[i];
 		}
 	}
+
+	//example input:
+	/*
+	ModPlug Tracker  IT
+|F-510......|A-510......|C-610......|...........|........D01|........M20
+|...........|...........|...........|...........|........D01|A-509......
+|===........|===........|===........|...........|........D01|...........
+|F-510......|A-510......|C-610......|...........|........D01|........D01
+|===..v32...|===..v32...|===..v32...|...........|D-609...F06|........D01
+|...........|...........|...........|...........|E-6.....G01|........D01
+|F-510...Z7E|A-510...Z7E|C-610...Z7E|...........|...........|........D01
+|........Z7C|........Z7C|........Z7C|...........|...........|D-609...F06
+	
+	
+	*/
+	//read from stdin or clipboard depending on options
+	std::string Input;
+	if (Options.USE_STDIN)
+	{
+		//do this later 
+	}
+	else
+	{
+		if (!OpenClipboard(NULL))
+		{
+			std::cout << "Failed to open clipboard";
+			return 1;
+		}
+		HANDLE hData = GetClipboardData(CF_TEXT);
+		if (hData == NULL)
+		{
+			std::cout << "Failed to get clipboard data";
+			return 1;
+		}
+		char* pszText = static_cast<char*>(GlobalLock(hData));
+		if (pszText == NULL)
+		{
+			std::cout << "Failed to lock clipboard data";
+			return 1;
+		}
+		Input = pszText;
+		GlobalUnlock(hData);
+		CloseClipboard();
+	}
+
+	std::string Format;
+	
+	Format = Input.substr(HEADER.length(), 3);
+
+	if (!(std::find(FORMATS_M.begin(), FORMATS_M.end(), Format) != FORMATS_M.end() || std::find(FORMATS_S.begin(), FORMATS_S.end(), Format) != FORMATS_S.end()))
+	{
+		std::cout << "Input does not contain OpenMPT pattern data.";
+		return 2;
+	}
+
+	//find occurences of \u001B\\[\\d+(;\\d+)*m and replace with ""
+	
+	Input = std::regex_replace(Input, std::regex("\u001B\\[\\d+(;\\d+)*m"), "");
+
+	std::cout << Input << std::endl;
+
+	
 
 	std::cout << "HELP: " << Options.HELP << std::endl;
 	std::cout << "USE_STDIN: " << Options.USE_STDIN << std::endl;
@@ -134,7 +197,7 @@ CLIOptions ParseCommandLine(int argc, char* argv[])
 				{
 					options.HELP = true;
 				}
-				else if (argv[i][j] == 's')
+				else if (argv[i][j] == 'i')
 				{
 					options.USE_STDIN = true;
 				}
@@ -166,4 +229,30 @@ std::vector<std::string> Split(const std::string& s, char delimiter)
 		tokens.push_back(token);
 	}
 	return tokens;
+}
+
+std::string GetSGRCode(int color)
+{
+	int n = color + ((color < 8) ? 30 : 82);
+	return "\u001B[" + std::to_string(n) + "m";
+}
+
+int GetNoteColor(char c)
+{
+	return (c >= 'A' && c <= 'G') ? 1 : 0;
+}
+
+int GetInstrumentColor(char c) {
+	return c >= '0' ? 2 : 0;
+}
+
+int GetEffectCmdColor(char c, std::string f) 
+{
+	int color = 0;
+	if (std::find(FORMATS_S.begin(), FORMATS_S.end(), f) != FORMATS_S.end())
+	{
+		//do this later
+	}
+	
+	return color;
 }
